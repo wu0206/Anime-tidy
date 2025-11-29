a222c18010a775cf3cd401e3ca832c5dd3aff229
wu0206 <92150388+wu0206@users.noreply.github.com>
2025-11-29 17:16:27 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 告訴 Tailwind 掃描 src 資料夾下的所有 React 檔案
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
